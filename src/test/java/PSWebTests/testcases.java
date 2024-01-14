aaa203a4861de782dc7e704d4c28eec086497820
AmanKandhola <amankandhola01@gmail.com>
2024-01-14 20:43:15 +0530

    this is the second case added with priority=2

--- src/test/java/PSWebTests/testcases.java
+++ src/test/java/PSWebTests/testcases.java
@@ -1,5 +1,9 @@
 package PSWebTests;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import org.testng.annotations.Test;
 
 import BaseClass.BaseSetup;
@@ -15,5 +19,12 @@ public class testcases extends BaseSetup{
 		ObjectL.clickToProceed();
 	}
 	
+	@Test(priority =2)
+	public void enterPhoneNumber2() {
+		Login ObjectL = new Login(obj);
+		ObjectL.EnterNumber("7889023442");
+		ObjectL.clickTORegister();
+		ObjectL.clickToProceed();
+	}
 	
 }
